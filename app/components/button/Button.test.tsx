d534f97076db60169cc433a7d67225a3606fa8bd
Travis Draper <tdraper.dev@gmail.com>
2023-04-12 14:50:22 -0400

    removed link related tests from Button test file

--- app/components/button/Button.test.tsx
+++ app/components/button/Button.test.tsx
@@ -1,9 +1,7 @@
 import { Button } from "./Button";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
 
 describe("Button", () => {
-  // button
   it("renders button when no type passed", () => {
     render(<Button />);
     expect(screen.getByRole("button")).toBeInTheDocument();
@@ -23,27 +21,5 @@ describe("Button", () => {
     expect(button).toHaveClass(
       "text-blue-800 bg-white hover:bg-blue-50 border"
     );
-  });
-
-  // link
-  it("renders link when type passed", () => {
-    render(<Button to="/" type="link" />, { wrapper: MemoryRouter });
-    expect(screen.getByRole("link")).toBeInTheDocument();
-  });
-
-  it("renders default primary styling on link", () => {
-    render(<Button to="/" type="link" />, { wrapper: MemoryRouter });
-    const link = screen.getByRole("link");
-    expect(link).toHaveClass(
-      "text-white bg-blue-700 hover:bg-blue-800 focus:bg-blue-600"
-    );
-  });
-
-  it("renders secondary styling when variant passed to link", () => {
-    render(<Button to="/" type="link" variant="secondary" />, {
-      wrapper: MemoryRouter,
-    });
-    const link = screen.getByRole("link");
-    expect(link).toHaveClass("text-blue-800 bg-white hover:bg-blue-50 border");
   });
 });
