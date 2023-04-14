359724afac3520147f020bf01c69853e4155b692
Travis Draper <tdraper.dev@gmail.com>
2023-04-14 14:36:17 -0400

    created modlet folder and test and story for dropdown component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,30 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node", "@remix-run/eslint-config/jest-testing-library", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
   env: {
-    "cypress/globals": true
+    "cypress/globals": true,
   },
-  plugins: ["cypress"],
+  plugins: ["cypress", "testing-library"],
+  rules: {
+    "testing-library/no-await-sync-events": [
+      "error",
+      {
+        eventModules: ["fire-event"],
+      },
+    ],
+  },
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but it means we have to explicitly
   // set the jest version.
   settings: {
     jest: {
-      version: 28
-    }
-  }
+      version: 28,
+    },
+  },
 };
