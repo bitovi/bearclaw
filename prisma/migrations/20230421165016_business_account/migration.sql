41a638016b43c134a06c4ead4acf2752808f4381
Ryan Spencer <rspencer@bitovi.com>
2023-05-25 17:22:43 -0700

    move to postgres and docker

--- prisma/migrations/20230421165016_business_account/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "BusinessAccount" (
-    "accountId" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "BusinessAccount_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "BusinessAccount_userId_key" ON "BusinessAccount"("userId");
