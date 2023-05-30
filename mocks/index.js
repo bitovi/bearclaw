9970037a1c8aecdd840c896f07523b3c3d6d6ebe
Travis Draper <tdraper.dev@gmail.com>
2023-05-30 15:05:09 -0400

    env

--- mocks/index.js
+++ mocks/index.js
@@ -1,21 +1,19 @@
+require("dotenv").config();
+
 const { setupServer } = require("msw/node");
 const { rest } = require("msw");
 const fixture_getRSBOMSCyclonedx = require("./mockData/getRSBOMSCyclonedx.ts");
-const fixture_getAllParentJobs = require("./mockData/getAllParentJobs.js");
+const fixture_getAllParentJobs = require("./mockData/getAllParentJobs.ts");
+
+const baseURL = process.env.BEAR_CLAW_SERVER;
 
 const handlers = [
-  rest.get(
-    `http://44.214.117.70:5001/claw/get_rsboms_cyclonedx`,
-    (req, res, ctx) => {
-      return res(ctx.json(fixture_getRSBOMSCyclonedx));
-    }
-  ),
-  rest.get(
-    "http://44.214.117.70:5001/claw/get_all_parent_jobs",
-    (req, res, ctx) => {
-      return res(ctx.json(fixture_getAllParentJobs));
-    }
-  ),
+  rest.get(`${baseURL}/claw/get_rsboms_cyclonedx`, (req, res, ctx) => {
+    return res(ctx.json(fixture_getRSBOMSCyclonedx));
+  }),
+  rest.get(`${baseURL}/claw/get_all_parent_jobs`, (req, res, ctx) => {
+    return res(ctx.json(fixture_getAllParentJobs));
+  }),
 ];
 
 const server = setupServer(...handlers);
