885c83600fc0feec798cf517b5e784ec377ecd5c
Travis Draper <tdraper.dev@gmail.com>
2023-06-15 13:12:59 -0600

    refactored Table search filter logic to import reusable hook that leverages URL moreso than local state

--- app/components/table/NavigationFilter.tsx
+++ app/components/table/NavigationFilter.tsx
@@ -10,51 +10,9 @@ import {
 import FilterListIcon from "@mui/icons-material/FilterList";
 
 import { TextInput } from "../input";
-import { Form, useNavigate, useSearchParams } from "@remix-run/react";
-import { useCallback, useMemo, useState } from "react";
+import { Form } from "@remix-run/react";
 import type { DropdownOption } from "./Table";
-import { useDebounceApiCall } from "~/hooks/useDebounceApiCall";
-import { buildNewSearchParams } from "~/utils/buildNewSearchParams";
-import { parseFilterParam } from "~/utils/parseFilterParam";
-
-function useFiltering() {
-  const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
-  const { _searchField, _searchString } = parseFilterParam(
-    searchParams.get("filter")
-  );
-  const [searchString, setSearchString] = useState(_searchString || "");
-  const [searchField, setSearchField] = useState(_searchField || "");
-
-  const updatedSearchParams = useMemo(() => {
-    return buildNewSearchParams(searchParams, {
-      filter: `contains(${searchField},${searchString})`,
-    });
-  }, [searchString, searchField, searchParams]);
-
-  const handleSearchString = (val: string) => {
-    setSearchString(val);
-  };
-
-  const handleSearchField = (val: string) => {
-    setSearchField(val);
-  };
-
-  const apiCall = useCallback(() => {
-    navigate(`./?${updatedSearchParams}`);
-  }, [updatedSearchParams, navigate]);
-
-  useDebounceApiCall({
-    apiCall,
-  });
-
-  return {
-    searchField,
-    searchString,
-    handleSearchString,
-    handleSearchField,
-  };
-}
+import { useFiltering } from "~/hooks/useFiltering";
 
 export function NavigationFilter({
   dropdownOptions: _dropdownOptions,
@@ -65,8 +23,7 @@ export function NavigationFilter({
   dropdownLabel: string;
   searchLabel: string;
 }) {
-  const { searchField, searchString, handleSearchString, handleSearchField } =
-    useFiltering();
+  const { searchField, searchString, debounceFilterQuery } = useFiltering();
 
   const dropdownOptions = [{ value: "", label: "Select a Field" }].concat(
     _dropdownOptions
@@ -81,32 +38,41 @@ export function NavigationFilter({
             inputProps={{
               sx: { maxHeight: "20px" },
             }}
-            onChange={({ target }) => handleSearchString(target.value)}
+            onChange={({ target }) =>
+              debounceFilterQuery({ searchString: target.value })
+            }
             label={searchLabel}
-            value={searchString}
+            defaultValue={searchString || ""}
             sx={{ minWidth: "200px" }}
           />
-          <FormControl fullWidth>
-            <InputLabel id={`filter-${dropdownLabel}-select`}>
-              {dropdownLabel}
-            </InputLabel>
-            <Select
-              onChange={({ target }) => handleSearchField(target.value)}
-              labelId={`filter-${dropdownLabel}-select-label`}
-              id={`filter-${dropdownLabel}-select`}
-              value={searchField}
-              displayEmpty
-              label={dropdownLabel} // To ensure MUI notched outline styling, we still need to pass a value to the label prop
-            >
-              {dropdownOptions.map((opt) => {
-                return (
-                  <MenuItem key={opt.value} value={opt.value}>
-                    {opt.label}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
+          {dropdownOptions.length && (
+            <FormControl fullWidth>
+              <InputLabel id={`filter-${dropdownLabel}-select`}>
+                {dropdownLabel}
+              </InputLabel>
+              <Select
+                onChange={({ target }) =>
+                  debounceFilterQuery({ searchField: target.value })
+                }
+                labelId={`filter-${dropdownLabel}-select-label`}
+                id={`filter-${dropdownLabel}-select`}
+                value={
+                  dropdownOptions.find((r) => r.value === searchField)?.value ||
+                  ""
+                }
+                displayEmpty
+                label={dropdownLabel} // To ensure MUI notched outline styling, we still need to pass a value to the label prop
+              >
+                {dropdownOptions.map((opt) => {
+                  return (
+                    <MenuItem key={opt.value} value={opt.value}>
+                      {opt.label}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+          )}
           <IconButton>
             <FilterListIcon />
           </IconButton>
