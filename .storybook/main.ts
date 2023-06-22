f71af75de26de1b4949129657f3d6a6963b9651e
Travis Draper <tdraper.dev@gmail.com>
2023-06-22 15:13:20 -0600

    removed unnecessary addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,6 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-addon-react-router-v6",
-    "storybook-addon-mock",
   ],
   framework: {
     name: "@storybook/react-vite",
