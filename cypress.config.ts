cb668132e5c12677d78bfc1c95b1013a1060659e
Travis Draper <tdraper.dev@gmail.com>
2023-07-11 14:11:29 -0400

    config update

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,7 @@ const { rmdir, existsSync } = require("fs");
 
 export default defineConfig({
   e2e: {
-    experimentalSessionAndOrigin: true,
-    testIsolation: "on",
+    testIsolation: true,
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
       const port = process.env.PORT ?? (isDev ? "3000" : "8811");
