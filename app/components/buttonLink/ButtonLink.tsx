01a2bd9ff16db507bd83901eca0d00d198a76c71
Travis Draper <88675789+tdraper-dev@users.noreply.github.com>
2023-07-14 16:40:16 -0400

    fix/Update Prefetch on ButtonLink (#105)
    * updated the logic for our ButtonLink to default to "intent" if no value is provided, then updated the ButtonLink on verifyEmail to have "none" as its prefetch
    
    * fixed lint and typecheck
    
    * tweak to interface

--- app/components/buttonLink/ButtonLink.tsx
+++ app/components/buttonLink/ButtonLink.tsx
@@ -1,12 +1,21 @@
 import Button from "@mui/material/Button";
 import type { ButtonProps } from "@mui/material/Button";
+import type { LinkProps } from "@remix-run/react";
 import { Link } from "@remix-run/react";
 
-type Props = ButtonProps & {
-  to?: string;
-};
+type Props = ButtonProps &
+  Omit<LinkProps, "to"> & {
+    to?: string;
+  };
 
-export function ButtonLink({ to, children, variant, sx, ...props }: Props) {
+export function ButtonLink({
+  to,
+  children,
+  variant,
+  sx,
+  prefetch,
+  ...props
+}: Props) {
   return (
     <Button
       disableRipple
@@ -14,7 +23,7 @@ export function ButtonLink({ to, children, variant, sx, ...props }: Props) {
       component={to ? Link : "div"}
       aria-disabled={!to}
       to={to}
-      prefetch="intent"
+      prefetch={prefetch ?? "intent"}
       variant={variant}
       sx={{
         ...sx,
