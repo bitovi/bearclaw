5629f45e200493f5c81abe76f071883cab4e0eb1
Ryan Spencer <rspencer@bitovi.com>
2023-08-04 12:37:42 -0500

    auth not required in production, so remove the requirement

--- app/services/bigBear/bearFetch.server.ts
+++ app/services/bigBear/bearFetch.server.ts
@@ -1,17 +1,18 @@
 import invariant from "tiny-invariant";
 
 export const bearFetch = async (path: string, init?: RequestInit) => {
-  invariant(process.env.BEARCLAW_AUTH, "BEARCLAW_AUTH must be set");
   invariant(process.env.BEARCLAW_URL, "SESSION_SECRET must be set");
 
-  const AUTH_TOKEN = Buffer.from(process.env.BEARCLAW_AUTH).toString("base64");
   const BASE_URL = process.env.BEARCLAW_URL;
+  const AUTH_TOKEN =
+    process.env.BEARCLAW_AUTH &&
+    Buffer.from(process.env.BEARCLAW_AUTH).toString("base64");
 
   return fetch(`${BASE_URL}${path}`, {
     ...init,
     headers: {
       ...init?.headers,
-      Authorization: `Basic ${AUTH_TOKEN}`,
+      ...(AUTH_TOKEN ? { Authorization: `Basic ${AUTH_TOKEN}` } : {}),
     },
   });
 };
