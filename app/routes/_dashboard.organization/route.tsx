82c3e7cb2e64054fe528ccbcab28891b025be08f
Travis Draper <tdraper.dev@gmail.com>
2023-08-14 09:15:59 -0400

    added the new organization route with catch boundary and orgUser check in loader

---
+++ app/routes/_dashboard.organization/route.tsx
@@ -0,0 +1,50 @@
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { json } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
+import { Outlet, isRouteErrorResponse, useRouteError } from "@remix-run/react";
+import { retrieveActiveOrganizationUser } from "~/models/organizationUsers.server";
+import { getOrgandUserId } from "~/session.server";
+import Box from "@mui/material/Box";
+
+export async function loader({ request }: LoaderArgs) {
+  const { userId, organizationId } = await getOrgandUserId(request);
+  const orgUser = await retrieveActiveOrganizationUser({
+    organizationId,
+    userId: userId,
+  });
+
+  if (!orgUser) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({});
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  // when true, this is what used to go to `CatchBoundary`
+  if (isRouteErrorResponse(error)) {
+    return (
+      <Stack width="100%" height="100%" alignItems="center">
+        <Box paddingTop={4} textAlign="center">
+          <Typography variant="h4">Oops!</Typography>
+          <Typography variant="body1">Something went wrong</Typography>
+        </Box>
+      </Stack>
+    );
+  }
+
+  return (
+    <Stack width="100%" height="100%" alignItems="center">
+      <Box paddingTop={4} textAlign="center">
+        <Typography variant="h4">Oops!</Typography>
+        <Typography variant="body1">Something went wrong</Typography>
+      </Box>
+    </Stack>
+  );
+}
+
+export default function Route() {
+  return <Outlet />;
+}
