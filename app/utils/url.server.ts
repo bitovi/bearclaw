a8fe9baa652162e47e4a0d0d8ed103a9c76338ac
Ryan Spencer <rspencer@bitovi.com>
2023-08-16 08:53:43 -0500

    not error for cypress

--- app/utils/url.server.ts
+++ app/utils/url.server.ts
@@ -2,7 +2,8 @@ export function getDomainUrl(request: Request) {
   const host =
     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
   if (!host) {
-    throw new Error("Could not determine domain URL.");
+    console.error("Could not determine domain URL.");
+    return "";
   }
   const protocol = host.includes("localhost") ? "http" : "https";
   return `${protocol}://${host}`;
